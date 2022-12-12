cb2b86826f4106a485e0db9aa056520552518802
emsiru <119374566+emsiru@users.noreply.github.com>
2022-12-12 11:13:48 +0800

    Create header.py

---
+++ pages/header.py
@@ -0,0 +1,10 @@
+from dash import app as app
+from dash import dash, dcc, html
+import dash_bootstrap_components as dbc
+from opcua import Client
+import dash_daq as daq
+
+layout = html.Div([ 
+    html.H1('Hey')
+])
+
